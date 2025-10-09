c167535b6d3afb7fb7612c0515a36a4d26f7ac62
mitty-ambinintsoa <rado23.andria@gmail.com>
2025-10-09 20:53:01 +0300

    train

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php 
 header("location:./pages/index.php");
+
+//git init
+// git add.
+// git commit -m "texte ze tiko";
+// git config --global user.name "mon nom"
+// git config --global user.email "rado23.andria@gmail.com"
+// => quand on a pas de brance alors on en mettre
+// * master
+//git branch -M main
+//git push -u origin main
